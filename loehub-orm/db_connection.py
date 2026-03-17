82a1c0f7d25ac753343c684742d43f0ca1fa7f23
abishxk <107830484+abishxk@users.noreply.github.com>
2026-03-18 01:17:08 +0530

    Delete loehub-orm directory

--- loehub-orm/db_connection.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = "postgresql://postgres:root%40123@localhost:5432/loehub"
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
